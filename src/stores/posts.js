f13f296c7a977752e0ac308967b94ec54c77cd96
Ana Martins <550368+anamartins@users.noreply.github.com>
2022-12-12 17:37:48 +0100

    infinite scroll

--- src/stores/posts.js
+++ src/stores/posts.js
@@ -1,55 +1,49 @@
 import { defineStore } from 'pinia'
 import axios from 'axios'
+import { ref } from 'vue'
 
 const API_BASE_URL =
   'https://us-central1-rss-reader-365617.cloudfunctions.net/api'
 
-// export const usePostsStore = defineStore('posts', {
-//   state: () => ({
-//     posts: [],
-//     total: 0
-//   }),
-//   actions: {
-//     getPosts() {
-//       const returnAPI = axios.get(`${API_BASE_URL}/stream`)
-//     }
-
-//     //  this.posts = returnAPI.items;
-//     //  this.total = returnAPI.total;
-//   }
-// })
-
 export const usePostsStore = defineStore('posts', () => {
   const token = localStorage.getItem('mineToken')
+  const posts = ref([])
+  const total = ref(0)
+  const next = ref('')
+  const isReady = ref(false)
 
   async function getPosts(feedId) {
     let arg = ''
     if (feedId) {
       arg = `?feedId=${feedId}`
     }
-
+    isReady.value = false
     const returnAPI = await axios.get(`${API_BASE_URL}/stream${arg}`, {
       headers: {
         Authorization: `Bearer ${token}`
       },
       withCredentials: true
     })
-    this.posts = returnAPI.data.items
-    this.total = returnAPI.data.total
-    this.next = returnAPI.data.next
+    posts.value = returnAPI.data.items
+    total.value = returnAPI.data.total
+    next.value = returnAPI.data.next
+    isReady.value = true
   }
 
   async function getMorePosts() {
+    isReady.value = false
     const returnAPI = await axios.get(this.next, {
       headers: {
         Authorization: `Bearer ${token}`
       },
       withCredentials: true
     })
-    this.posts = returnAPI.data.items
-    this.total = returnAPI.data.total
-    this.next = returnAPI.data.next
+    posts.value = [...posts.value, ...returnAPI.data.items]
+
+    total.value += returnAPI.data.total
+    next.value = returnAPI.data.next
+    isReady.value = true
   }
 
-  return { getPosts, getMorePosts }
+  return { getPosts, getMorePosts, posts, total, next, isReady }
 })
--- src/views/home/stream.vue
+++ src/views/home/stream.vue
@@ -1,24 +1,37 @@
 <script setup>
-import { onMounted, computed, defineProps } from 'vue'
+import { onMounted, onUnmounted, computed, ref, defineProps } from 'vue'
 import { usePostsStore } from '../../stores/posts'
 
 import Post from './post.vue'
 
+const box = ref(null)
+
+onMounted(() => {
+  let observer = new IntersectionObserver(onObserverChanges)
+  let target = box.value
+  observer.observe(target)
+})
+
 let postsStore = usePostsStore()
-// postsStore.getPosts()
 let posts = computed(() => postsStore.posts)
+let isReady = computed(() => postsStore.isReady)
 
 function onButtonClick() {
   postsStore.getMorePosts()
   console.log('hoy')
 }
 
-onMounted(() => {})
+function onObserverChanges(entries) {
+  let isIntersecting = entries[0].isIntersecting
+  if (isReady.value && isIntersecting) postsStore.getMorePosts()
+}
 </script>
 <template>
   <div class="stream">
+    <div class="start" ref="start"></div>
     <Post v-for="post in posts" :post="post" :key="post.id" />
-    <button type="button" @click="onButtonClick">NEXT</button>
+    <!-- <button type="button" @click="onButtonClick">NEXT</button> -->
+    <div class="box" ref="box"></div>
   </div>
 </template>
 
@@ -26,5 +39,9 @@ onMounted(() => {})
 .stream {
   position: relative;
   width: 70%;
+}
+
+.box {
+  height: 5px;
 }
 </style>
