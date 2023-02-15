10541996af17101dbc1613d975470e0db459a8ad
Ana Martins <550368+anamartins@users.noreply.github.com>
2023-02-15 19:25:13 +0100

    add case insesitive name sorting

--- src/stores/feeds.js
+++ src/stores/feeds.js
@@ -11,15 +11,14 @@ export const useFeedsStore = defineStore('feeds', () => {
   const { getApi, postApi } = useApi()
 
   function sortFeeds() {
-    console.log('store!', feeds.value)
-    // feeds.value = feeds.value.map((e) => console.log('map!', e))
-    feeds.value = feeds.value.sort(function (a, b) {
-      if (a.title < b.title) {
-        return -1
-      }
-      return 0
+    feeds.value.sort(function (a, b) {
+      // if (a.title.toLowerCase() < b.title.toLowerCase()) {
+      //   return -1
+      // }
+      // return 0
+
+      return a.title.toLowerCase().localeCompare(b.title.toLowerCase())
     })
-    console.log('after', feeds.value)
   }
 
   function getFeedById(id) {
@@ -51,7 +50,6 @@ export const useFeedsStore = defineStore('feeds', () => {
     try {
       isLoading.value = true
       const returnAPI = await getApi(`user/feeds`, {})
-
       feeds.value = returnAPI.data.feeds
       sortFeeds()
       total.value = returnAPI.data.total
